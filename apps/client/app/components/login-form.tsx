75023fb088575f4b48b3e55b555d304437ab3da6
Hammad Majid <hammadmajid@proton.me>
2025-06-14 13:28:42 +0500

    feat: implement form handling and validation for login and registration forms

--- apps/client/app/components/login-form.tsx
+++ apps/client/app/components/login-form.tsx
@@ -1,48 +1,86 @@
+'use client';
+
 import { cn } from '~/lib/utils';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
-import { Label } from '~/components/ui/label';
 import { Link } from 'react-router';
+import { loginSchema } from '@repo/schemas/user';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '~/components/ui/form';
+import z from 'zod';
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentProps<'form'>) {
-return (
-    <form className={cn('flex flex-col gap-6', className)} {...props}>
+export function LoginForm({ className }: React.ComponentProps<'form'>) {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn('flex flex-col gap-6', className)}
+      >
         <div className='flex flex-col items-center gap-2 text-center'>
-            <h1 className='text-2xl font-bold'>Login to your account</h1>
-            <p className='text-muted-foreground text-sm text-balance'>
-                Enter your credentials below to login to your account
-            </p>
+          <h1 className='text-2xl font-bold'>Login to your account</h1>
+          <p className='text-muted-foreground text-sm text-balance'>
+            Enter your credentials below to login to your account
+          </p>
         </div>
         <div className='grid gap-6'>
-            <div className='grid gap-3'>
-                <Label htmlFor='username'>Username</Label>
-                <Input id='username' type='text' required />
-            </div>
-            <div className='grid gap-3'>
-                <div className='flex items-center'>
-                    <Label htmlFor='password'>Password</Label>
-                    <a
-                        href='#'
-                        className='ml-auto text-sm underline-offset-4 hover:underline'
-                    >
-                        Forgot your password?
-                    </a>
-                </div>
-                <Input id='password' type='password' required />
-            </div>
-            <Button type='submit' className='w-full'>
-                Login
-            </Button>
+          <div className='grid gap-3'>
+            <FormField
+              control={form.control}
+              name='username'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder='shadcn' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className='grid gap-3'>
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type='submit' className='w-full' disabled={form.formState.isSubmitting}>
+            Login
+          </Button>
         </div>
         <div className='text-center text-sm'>
-            Don&apos;t have an account?{' '}
-            <Link to='/register' className='underline underline-offset-4'>
-                Sign up
-            </Link>
+          Don&apos;t have an account?{' '}
+          <Link to='/register' className='underline underline-offset-4'>
+            Sign up
+          </Link>
         </div>
-    </form>
-);
+      </form>
+    </Form>
+  );
 }
--- apps/client/app/components/register-form.tsx
+++ apps/client/app/components/register-form.tsx
@@ -1,50 +1,123 @@
+'use client';
+
 import { cn } from '~/lib/utils';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
-import { Label } from '~/components/ui/label';
 import { Link } from 'react-router';
+import { registrationSchema } from '@repo/schemas/user';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '~/components/ui/form';
+import z from 'zod';
 
-export function RegisterForm({
-  className,
-  ...props
-}: React.ComponentProps<'form'>) {
+export function RegisterForm({ className }: React.ComponentProps<'form'>) {
+  const form = useForm<z.infer<typeof registrationSchema>>({
+    resolver: zodResolver(registrationSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof registrationSchema>) {
+    console.log(values);
+  }
+
   return (
-    <form className={cn('flex flex-col gap-6', className)} {...props}>
-      <div className='flex flex-col items-center gap-2 text-center'>
-        <h1 className='text-2xl font-bold'>Create your account</h1>
-        <p className='text-muted-foreground text-sm text-balance'>
-          Enter your details below to create a new account
-        </p>
-      </div>
-      <div className='grid gap-6'>
-        <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
-          <div className='grid gap-3'>
-            <Label htmlFor='name'>Name</Label>
-            <Input id='name' type='text' required placeholder='John Doe' />
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn('flex flex-col gap-6', className)}
+      >
+        <div className='flex flex-col items-center gap-2 text-center'>
+          <h1 className='text-2xl font-bold'>Create your account</h1>
+          <p className='text-muted-foreground text-sm text-balance'>
+            Enter your details below to create a new account
+          </p>
+        </div>
+        <div className='grid gap-6'>
+          <div className='grid grid-cols-1 gap-3 sm:grid-cols-2'>
+            <div className='grid gap-3'>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='John Doe' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='grid gap-3'>
+              <FormField
+                control={form.control}
+                name='username'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder='shadcn' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
           </div>
           <div className='grid gap-3'>
-            <Label htmlFor='username'>Username</Label>
-            <Input id='username' type='text' required placeholder='johndoe' />
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      id='email'
+                      type='email'
+                      required
+                      placeholder='john@example.com'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
+          <div className='grid gap-3'>
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input id='password' type='password' required {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type='submit' className='w-full' disabled={form.formState.isSubmitting}>
+            Register
+          </Button>
         </div>
-        <div className='grid gap-3'>
-          <Label htmlFor='email'>Email</Label>
-          <Input id='email' type='email' required placeholder='john@example.com' />
+        <div className='text-center text-sm'>
+          Already have an account?{' '}
+          <Link to='/login' className='underline underline-offset-4'>
+            Login
+          </Link>
         </div>
-        <div className='grid gap-3'>
-          <Label htmlFor='password'>Password</Label>
-          <Input id='password' type='password' required />
-        </div>
-        <Button type='submit' className='w-full'>
-          Register
-        </Button>
-      </div>
-      <div className='text-center text-sm'>
-        Already have an account?{' '}
-        <Link to='/login' className='underline underline-offset-4'>
-          Login
-        </Link>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 }
