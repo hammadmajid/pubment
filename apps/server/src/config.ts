acdfc7a64550d98173a5056e9b3bde915908fd62
Hammad Majid <hammadmajid@proton.me>
2025-06-15 17:12:02 +0500

    fix: update dotenv to import .env from root of the project

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -1,4 +1,12 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+// @ts-expect-error import.meta is valid in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 const jwtSecret = process.env.JWT_SECRET;
 
