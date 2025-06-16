dcbb28a0c4f54484f2c20a150161ab59feabcc69
Hammad Majid <hammadmajid@proton.me>
2025-06-16 14:35:21 +0500

    feat: implement settings route with logout functionality

--- apps/client/app/routes/settings.tsx
+++ apps/client/app/routes/settings.tsx
@@ -1,10 +1,36 @@
+import { destroySession, getSession } from '~/session.server';
 import type { Route } from './+types/settings';
 import AppWrapper from '~/components/app/wrapper';
+import { Form, redirect } from 'react-router';
+import { Button } from '~/components/ui/button';
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get('Cookie'));
+
+  if (!session.has('token')) {
+    return redirect('/login');
+  }
+
+  return session.get('userId');
+}
 
 export default function Settings({ loaderData }: Route.ComponentProps) {
   return (
     <AppWrapper>
-      <div className='px-8 py-2'></div>
+      <div className='px-8 py-2'>
+        <Form method='post'>
+          <Button variant='destructive'>Logout</Button>
+        </Form>
+      </div>
     </AppWrapper>
   );
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  const session = await getSession(request.headers.get('Cookie'));
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await destroySession(session),
+    },
+  });
 }
