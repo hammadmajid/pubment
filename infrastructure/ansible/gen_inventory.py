ce55c16f435bdfe85685dfd1061d2edee9857481
Hammad Majid <hammadmajid@proton.me>
2025-06-20 03:47:09 +0500

    fix: generate frontend_vars.yml to be consumbed by frontend.yml using the python script

--- infrastructure/ansible/gen_inventory.py
+++ infrastructure/ansible/gen_inventory.py
@@ -5,10 +5,18 @@ with open('tf_outputs.json') as f:
 
 frontend_ip = data['frontend_instance_public_ip']['value']
 backend_ip = data['backend_instance_public_ip']['value']
+backend_private_ip = data['backend_instance_private_ip']['value']
 
-print(f"""[frontend]
+# Write inventory.ini
+with open('inventory.ini', 'w') as inv:
+    inv.write(f"""[frontend]
 frontend ansible_host={frontend_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/code-alpha.pem
 
 [backend]
 backend ansible_host={backend_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/code-alpha.pem ansible_ssh_common_args='-o ProxyJump=ubuntu@{frontend_ip}'
+""")
+
+# Write frontend_vars.yml
+with open('frontend_vars.yml', 'w') as varsfile:
+    varsfile.write(f"""backend_private_ip: "{backend_private_ip}"
 """)
