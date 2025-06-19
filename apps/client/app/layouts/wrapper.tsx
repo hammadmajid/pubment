4eba3433a25eadb2bbf464ee0f9347e68de967f7
Hammad Majid <hammadmajid@proton.me>
2025-06-19 20:24:39 +0500

    refactor: integrate ThemeProvider into App and remove it from AppWrapper

--- apps/client/app/layouts/wrapper.tsx
+++ apps/client/app/layouts/wrapper.tsx
@@ -3,7 +3,6 @@
 import { Outlet } from 'react-router';
 import { useNavigation } from 'react-router';
 import { AppSidebar } from '~/components/app/sidebar';
-import { ThemeProvider } from '~/components/theme-provider';
 import {
   SidebarInset,
   SidebarProvider,
@@ -14,24 +13,22 @@ export default function AppWrapper() {
   const navigation = useNavigation();
 
   return (
-    <ThemeProvider defaultTheme='system' attribute='class'>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className='flex items-center justify-between p-4'>
-            <SidebarTrigger />
-          </header>
-          <main
-            className={
-              navigation.state === 'loading'
-                ? 'opacity-30 pointer-events-none transition-opacity'
-                : 'transition-opacity'
-            }
-          >
-            <Outlet />
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className='flex items-center justify-between p-4'>
+          <SidebarTrigger />
+        </header>
+        <main
+          className={
+            navigation.state === 'loading'
+              ? 'opacity-30 pointer-events-none transition-opacity'
+              : 'transition-opacity'
+          }
+        >
+          <Outlet />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
