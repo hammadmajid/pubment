9fcfb6aea477043694a365989c7b6a6e96acf7a8
Hammad Majid <hammadmajid@proton.me>
2025-06-22 11:00:01 +0500

    feat: remove frontend configuration and update inventory and scripts for backend only deployment

--- infra/scripts/gen_inventory.py
+++ infra/scripts/gen_inventory.py
@@ -1,22 +1,12 @@
 import json
 
-with open('tf_outputs.json') as f:
+with open('../ansible/tf_outputs.json') as f:
     data = json.load(f)
 
-frontend_ip = data['frontend_vm_public_ip']['value']
 backend_ip = data['backend_vm_public_ip']['value']
-backend_private_ip = data['backend_vm_private_ip']['value']
 
 # Write inventory.ini
-with open('inventory.ini', 'w') as inv:
-    inv.write(f"""[frontend]
-frontend ansible_host={frontend_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/pubment.pem
-
-[backend]
-backend ansible_host={backend_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/pubment.pem ansible_ssh_common_args='-o ProxyJump=ubuntu@{frontend_ip}'
-""")
-
-# Write frontend_vars.yml
-with open('frontend_vars.yml', 'w') as varsfile:
-    varsfile.write(f"""backend_private_ip: "{backend_private_ip}"
+with open('../ansible/inventory.ini', 'w') as inv:
+    inv.write(f"""[backend]
+backend ansible_host={backend_ip} ansible_user=azureuser ansible_ssh_private_key_file=~/.ssh/pubment.pem'
 """)
